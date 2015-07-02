b7327752860e916c02a4fecc27388de8c7503ccc
Michael Williams <dinosaur@riseup.net>
2015-07-02 22:27:28 +1200

    normalize output to floats between -1.0 and 1.0

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var defined = require('defined')
 var Ndarray = require('ndarray')
 var getDataType = require('dtype')
 var toUint8 = require('buffer-to-uint8array')
+var rangeFit = require('range-fit')
 
 module.exports = audioReadStream
 
@@ -32,6 +33,7 @@ function audioReadStream (opts) {
   // get audio
   var audio = ps.stdout
     .pipe(parseRawAudio(opts))
+    .pipe(normalize(opts))
     .pipe(through.obj())
 
   // stash stderr on the audio stream
@@ -93,4 +95,29 @@ function getEncoding (dtype) {
 
 function getBits (dtype) {
   return getDataType(dtype).BYTES_PER_ELEMENT * 8
-} 
+}
+
+function normalize (opts) {
+  return through.obj(function (audioIn, enc, cb) {
+    var maxVal, minVal
+    switch (opts.dtype[0]) {
+      case 'u':
+        minVal = 0
+        maxVal = Math.pow(2, opts.bits) - 1
+        break
+      case 'i':
+        minVal = -Math.pow(2, opts.bits - 1)
+        maxVal = Math.pow(2, opts.bits - 1) - 1
+        break
+      case 'f':
+        // not necessary to normalize
+        // TODO: double check this on a system that supports floats from SoX
+        return cb(null, audioIn)
+    }
+    var audioOut = Ndarray(new Float32Array(audioIn.data.length), audioIn.shape)
+    for (var i = 0; i < audioOut.data.length; i++) {
+      audioOut.data[i] = rangeFit(audioIn.data[i], minVal, maxVal, -1.0, 1.0)
+    }
+    cb(null, audioOut)
+  })
+}
