21207519daedfd69d8ace1b42d0de6f4a6687475
Michael Williams <dinosaur@riseup.net>
2015-07-06 00:52:49 +1200

    simplify example

--- example.js
+++ example.js
@@ -2,11 +2,7 @@ var audioReadStream = require('./')
 var through = require('through2')
 var terminalBar = require('terminal-bar')
 
-var audio = audioReadStream({
-  buffer: 1024
-})
-
-audio
+audioReadStream()
 .pipe(through.obj(function (arr, enc, cb) {
   var data = [].slice.call(arr.data).slice(0, 128)
   cb(null, terminalBar(data) + "\n")
