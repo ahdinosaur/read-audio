9d34516a05011033f0eba7fb48ce34565e9f8712
Michael Williams <dinosaur@riseup.net>
2015-07-19 22:18:36 +1200

    re-write browser implementation to no longer depend on microphone-stream
    also fix hideous bug where audio processor would eventually be garbage
    collected, lol

--- browser.js
+++ browser.js
@@ -1,58 +1,94 @@
 var getUserMedia = require('getusermedia')
 var through = require('through2')
 var defined = require('defined')
-var micStream = require('microphone-stream')
 var Ndarray = require('ndarray')
 
 module.exports = readAudio
  
 function readAudio (opts, cb) {
-  opts = defaultOpts(opts)
-
-  createMicStream(opts, cb)
-}
-
-function defaultOpts (opts) {
   opts = defined(opts, {})
-  opts.channels = defined(opts.channels, 1)
-  opts.buffer = defined(opts.buffer, 1024)
-  opts.highWaterMark = defined(opts.highWaterMark, 1)
-  return opts
-}
 
-function createMicStream (opts, cb) {
-  getUserMedia({
-    video: false,
-    audio: true
-  }, function(err, stream) {
+  // get opts
+  var channels = defined(opts.channels, 1)
+  var buffer = defined(opts.buffer, 1024)
+  var highWaterMark = defined(opts.highWaterMark, 1)
+  var context = opts.context
+  if (context == null) {
+    var AudioContext = window.AudioContext || window.webkitAudioContext
+    context = new AudioContext
+  }
+  var input = opts.input
 
-    if (err) { return cb(err) }
+  createMicStream(cb)
 
-    var mic = micStream(stream, {
-      bufferSize: opts.buffer,
-      channels: opts.channels
+  function createMicStream (cb) {
+    getAudioInput(function(err, input) {
+      if (err) { return cb(err) }
+
+      var stream = audioInputToStream(input)
+
+      cb(null, stream)
     })
-    .pipe(micToSamples(opts))
-    
-    cb(null, mic)
-  })
-}
+  }
 
-function micToSamples (opts) {
-  return through.obj({
-    highWaterMark: opts.highWaterMark
-  }, function(channels, enc, cb) {
-    var samples = Ndarray(
-      new Float32Array(opts.buffer * opts.channels),
-      [opts.buffer, opts.channels]
-    )
-
-    for (var bi = 0; bi < opts.buffer; bi++) {
-      for (var ci = 0; ci < opts.channels; ci++) {
-        samples.set(bi * opts.channels, ci, channels[ci][bi])
-      }
+  function getAudioInput (cb) {
+    // if input given, use it
+    if (input) {
+      return cb(null, input)
     }
 
-    cb(null, samples)
-  })
+    // otherwise return microphone input
+    getUserMedia({
+      video: false,
+      audio: true
+    }, function(err, micStream) {
+      if (err) { return cb(err) }
+
+      input = context.createMediaStreamSource(micStream)
+
+      cb(null, input)
+    })
+  }
+
+  function audioInputToStream (input) {
+    var stream = through.obj({
+      highWaterMark: highWaterMark
+    })
+
+    var recorder = context.createScriptProcessor(buffer, channels, channels);
+
+    recorder.onaudioprocess = processInput(stream)
+
+    input.connect(recorder)
+    recorder.connect(context.destination)
+
+    // save reference to recorder,
+    // stops audio process from being GC'd
+    stream._recorder = recorder
+
+    return stream
+  }
+
+  function processInput (stream) {
+    return function onAudioProcess (e) {
+      var audioIn = e.inputBuffer
+      var numChannels = audioIn.numberOfChannels
+      var numSamplesPerChannel = audioIn.length
+
+      var samples = Ndarray(
+        new Float32Array(numSamplesPerChannel * numChannels),
+        [numSamplesPerChannel, numChannels]
+      )
+
+      for (var ci = 0; ci < numChannels; ci++) {
+        var channel = audioIn.getChannelData(ci)
+        for (var bi = 0; bi < numSamplesPerChannel; bi++) {
+          var sample = channel[bi]
+          samples.set(bi, ci, sample)
+        }
+      }
+
+      stream.write(samples)
+    }
+  }
 }
